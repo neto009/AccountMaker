a90524bf15f486b003759c41af02ce890cdb5a19
Solange Neto <solange.neto@mitraecp.com>
2022-07-17 01:33:20 -0300

    Ajustes Box

--- src/sections/BoxSocial.js
+++ src/sections/BoxSocial.js
@@ -8,7 +8,21 @@ export default function BoxSocial() {
         <div className='border_1'></div>
         <div className='borderTitleText'>
             <div className='infoBar'>
-                <a className='infoBarBlock' href='https://www.twitch.tv/directory/game/Tibia'><div className='infoBarBigLogo'></div></a>
+                <a className='infoBarBlock' href='https://www.twitch.tv/directory/game/Tibia'>
+                  <img className='infoBarBigLogo' src='/img/icon/icon-twitch.png' alt='Twitch Icon'/>
+                </a>
+                <a className='infoBarBlock' href='https://www.youtube.com/channel/UCg5vFOB3tN8KGcJDyk6QQzQ/home'>
+                  <img className='infoBarBigLogo' src='/img/icon/icon-youtube.png' alt='Youtube Icon'/>
+                </a>
+                <a className='infoBarBlock' href='https://booyah.live/games/10019'>
+                  <img className='infoBarBigLogo' src='/img/icon/icon-booyah.png' alt='Booyah Icon'/>
+                </a>
+                <a className='infoBarOnlinePlayers' href='/#'>
+                  <img className='infoBarBigLogo' src='/img/icon/icon-players-online.png' alt='Players Icon'/>
+                  <span className='infoBarNumbers'>
+                    <span className='infoBarSmallElement'>0 Players Online</span>
+                  </span>
+                </a>
             </div>
         </div>
         <div className='border_1'></div>
